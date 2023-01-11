1e5e44747501e5fd734722a4ce858babbb9496a1
CastellucciLuca <castellucciluca98@gmail.com>
2023-01-11 15:10:11 +0100

    generated cards+img+Archetype

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,42 @@
+<script>
+import { store } from '../store.js';
+import axios from 'axios';
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            store,
+            apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=12&offset=0',
+        }
+    },
+    methods: {
+        getApiElement() {
+            axios.get(this.apiUrl,{
+                params:{
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.data);
+                    this.store.yuGiOhCards = response.data.data
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    },
+    created() {
+        this.getApiElement()
+    },
+}
+</script>
+
+<template>
+    <header>
+        <div class="container-fluid">
+            <h1 class="py-3">Yu-Gi-Oh</h1>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../store.js';
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <section id="cards">
+            <article class="single-card" v-for="cardItem in store.yuGiOhCards">
+                <img :src="cardItem.card_images[0].image_url" :alt="cardItem.name">
+                <h3>{{ cardItem.name }}</h3>
+                <span>{{ cardItem.archetype }}</span>
+            </article>
+        </section>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+</style>
